a49bec510e592c1966a7a4f22b7ca4dfce8cc5d4
Rasika Wedaarachchi <wedaarachchirasika@gmail.com>
2025-03-17 23:46:41 +0530

    Create Home.jsx file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from "../components/nav.jsx"
+
+function Home() {
+  return (
+    <div>
+        <Nav/>
+      Hiii ...
+    </div>
+  )
+}
+
+export default Home
