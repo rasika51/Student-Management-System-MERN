7061b163d329f211da518479d42ec63f7d563e41
Rasika Wedaarachchi <wedaarachchirasika@gmail.com>
2025-03-17 23:45:36 +0530

    Create Nav bar

---
+++ src/components/nav.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserButton } from "@clerk/clerk-react";
+
+const NavBar = () => {
+  return (
+    <div className="bg-black p-4">
+      <nav className="flex justify-between items-center">
+        <div className="text-white text-2xl font-bold">LMS</div>
+        <ul className="flex space-x-6">
+          <li className="mt-3">
+            <Link to="/" className="text-white hover:text-gray-400">
+              Home
+            </Link>
+          </li>
+          <li className="mt-3">
+            <Link to="/dashboard" className="text-white hover:text-gray-400">
+              Dashboard
+            </Link>
+          </li>
+          <li className="mt-3">
+            <Link to="/members" className="text-white hover:text-gray-400">
+              Members
+            </Link>
+          </li>
+          <li className="mt-3">
+            <Link to="/about" className="text-white hover:text-gray-400">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <button className="text-white hover:text-gray-400">
+              <UserButton />
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
