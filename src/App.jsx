e561e48fa79b964b03b2ef8a32a0b9df9390105e
Rasika Wedaarachchi <wedaarachchirasika@gmail.com>
2025-03-18 13:40:37 +0530

    Update App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Hero from "./pages/hero.jsx";
 import Home from "./pages/Home.jsx"
 
 import './App.css'
+import Update from './pages/Update.jsx';
+import View from './pages/View.jsx';
+import Dlte from './pages/dlte.jsx';
+import Add from './pages/add.jsx';
 
 function App() {
   
@@ -13,6 +17,10 @@ function App() {
         {/* <Route path="login" element={<Login />} /> */}
         <Route path="/" element={<Hero />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/update" element={<Update />} />
+        <Route path="/view" element={<View />} />
+        <Route path="/delete" element={<Dlte />} />
+        <Route path="/add" element={<Add />} />
 
 
 
